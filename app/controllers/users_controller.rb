db1db618713a6b68e09533ee9de5da0442d1a70c
Alice Boone <alicegbonel@gmail.com>
2020-11-28 18:26:27 -0800

    add method create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,26 +11,22 @@ class UsersController < ApplicationController
   def login_form
   end
 
-  def login
-    username = params[:username]
-    if username and user = User.find_by(username: username)
-      session[:user_id] = user.id
-      flash[:status] = :success
-      flash[:result_text] = "Successfully logged in as existing user #{user.username}"
+  def create
+    auth_hash = request.env["omniauth.auth"]
+    user = User.find_by(uid: auth_hash[:uid], provider: "github")
+    if user
+      flash[:notice] = "Existing user #{user.username} is logged in."
     else
-      user = User.new(username: username)
+      user = User.build_from_github(auth_hash)
       if user.save
-        session[:user_id] = user.id
-        flash[:status] = :success
-        flash[:result_text] = "Successfully created new user #{user.username} with ID #{user.id}"
+        flash[:success] = "Logged in back #{user.username}"
       else
-        flash.now[:status] = :failure
-        flash.now[:result_text] = "Could not log in"
-        flash.now[:messages] = user.errors.messages
-        render "login_form", status: :bad_request
-        return
+        flash[:error] = "could not create new user account: #{user.errors.messages}"
+        return redirect_to root_path
       end
     end
+
+    session[:user_id] = user.id
     redirect_to root_path
   end
 
