7c7f235aa0574c1ab26804c66c68b9c2ea74b184
Alice Boone <alicegbonel@gmail.com>
2020-11-28 20:39:08 -0800

    fixing error in the application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
 
   before_action :find_user, :current_user
 
-      def render_404
+  def render_404
     return render :file => "#{Rails.root}/public/404.html", :status => 404, :layout => false
   end
 
