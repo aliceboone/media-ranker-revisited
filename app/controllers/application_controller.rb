c9e0751394de8bedcaf49f1aa8c401a7f2f368b8
Alice Boone <alicegbonel@gmail.com>
2020-11-28 18:24:06 -0800

    Add current user and fixing application view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,23 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  before_action :find_user
+  before_action :find_user, :current_user
 
-  def render_404
+      def render_404
     return render :file => "#{Rails.root}/public/404.html", :status => 404, :layout => false
+  end
+
+  before_action :current_user
+
+  def current_user
+    return @current_user = User.find_by(id: session[:user_id])
+  end
+
+  def require_login
+    if current_user.nil?
+      flash[:error] = "A problem occurred: You must log in to do that"
+      redirect_to root_path
+    end
   end
 
   private
